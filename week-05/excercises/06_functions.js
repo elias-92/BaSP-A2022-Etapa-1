912bc39633f37c057554faa94990b1b6cffcdcbb
Elias Martinez <martinezelias17@gmail.com>
2022-09-20 12:31:31 -0300

    BaSP-W05-2022:ex06-functions

--- week-05/excercises/06_functions.js
+++ week-05/excercises/06_functions.js
@@ -5,7 +5,7 @@ Ejecutar la función y guardar el resultado en una variable, mostrando el valor 
 variable en la consola del navegador.*/
 console.log('\n-Excercises 6.a');
 function sum(num1, num2) {
-    return num1 + num2
+    return num1 + num2;
 }
 var result = sum(2,5);
 console.log('The result is: ' + result);
@@ -16,15 +16,15 @@ error y retornar el valor NaN como resultado.*/
 console.log('\n-Excercises 6.b');
 function validationNumber(num1, num2) {
     if (typeof num1 !== 'number') {
-        alert('-Excercises 6.b. The values are not number, please enter a number: ' + num1)
+        alert('-Excercises 6.b. The values are not number, please enter a number: ' + num1);
         return NaN;
-    }
-    if (typeof num2 !== 'number'){
+    } else if (typeof num2 !== 'number'){
         alert('-Excercises 6.b. The values are not number, please enter a number: ' + num2);
         return NaN;
+    } else{
+        return sum(num1, num2);
     }
-    return sum(num1, num2);        
-}
+};
 console.log('The entered values are incorrect, the result is: ' + validationNumber(2, '10'));
 console.log('The entered values are correct, the result is: ' + validationNumber(2, 10));
 
@@ -32,8 +32,8 @@ console.log('The entered values are correct, the result is: ' + validationNumber
 si es un número entero.*/
 console.log('\n-Excercises 6.c');
 function validateInteger(num) {
-    return Number.isInteger(num)      
-}
+    return Number.isInteger(num);   
+};
 console.log('you entered an incorrect value, the result is: ' + validateInteger('5'));
 console.log('you entered an correct value, the result is:' + validateInteger(5));
 
@@ -43,25 +43,25 @@ número convertido a entero (redondeado).*/
 console.log('\n-Excercises 6.d');
 function sumAndValidation(num1, num2) {
     if (typeof num1 !== 'number') {
-        alert('-Excercises 6.d. The values are not number, please enter a number: ' + num1)
+        alert('-Excercises 6.d. The one value is not number, please enter a number: ' + num1);
         return NaN;
-    }else if (!validateInteger(num1)){
-        alert('-Excercises 6.d \n Not a integer number ' + num1 + '\n we round it to: ' + Math.round(num1));
-        num1 = Math.round(num1);
-        return sum(num1, num2);
     }
     if (typeof num2 !== 'number'){
-        alert('-Excercises 6.d. The values are not number, please enter a number: ' + num2);
+        alert('-Excercises 6.d. The second value is not number, please enter a number: ' + num2);
         return NaN;
-    }else if (!validateInteger(num2)){
+    }
+    if (!validateInteger(num1)){
+        alert('-Excercises 6.d \n Not a integer number ' + num1 + '\n we round it to: ' + Math.round(num1));
+        num1 = Math.round(num1);
+    }
+    if (!validateInteger(num2)){
         alert('-Excercises 6.d \n Not a integer number ' + num2 + '\n we round it to: ' + Math.round(num2));
         num2 = Math.round(num2);
-        return sum(num1, num2);
     }
-    return sum(num1, num2);
-}
+        return sum(num1, num2);
+};
 console.log('The result with wrong value is: ' + sumAndValidation('2.4', 5) );
-console.log('The result with rounded value is: ' + sumAndValidation(4.6, 5) );
+console.log('The result with rounded value is: ' + sumAndValidation(4.6, 5.8) );
 
 /*Convertir la validación del ejercicio 6d en una función separada y llamarla dentro de la función suma 
 probando que todo siga funcionando igual.*/
@@ -74,14 +74,15 @@ function validation(num) {
     }else if (!validateInteger(num)){
         alert('-Excercises 6.e \n Not a integer number ' + num + '\n we round it to: ' + Math.round(num));
         return Math.round(num);
+    } else{
+        return num;
     }
-    return num;
 };
 
 function sum1(num1, num2) {
     num1 = validation(num1);
     num2 = validation(num2);
     return num1 + num2;
-}
+};
 
-console.log('The result of exercise 6.e is: ' + sum1(2.7, 13));
+console.log('The result of exercise 6.e is: ' + sum1(2.7, 13.7));
